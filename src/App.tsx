1c3c5b8682cf588c3c80b303f1e61312f133bfcb
IJTINK7 <ijtink7@mail.ru>
2022-11-08 06:48:31 +0300

    Create "Greeting" component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Greeting from "./Greeting";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello, samurai! Let's go!</h1>
+			<Greeting/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Greeting.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Greeting = () => {
+	return (
+		<div>
+			<div>
+				<h2>Hello, friend!</h2>
+				<h3>What to learn</h3>
+			</div>
+			<div>
+				<ol>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JS</li>
+				</ol>
+			</div>
+		</div>
+	);
+}
+export default Greeting;
