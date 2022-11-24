91809aa16ed446a4994eec52f34ab94598cd22bf
IJTINK7 <ijtink7@mail.ru>
2022-11-24 19:47:55 +0300

    Training with filter method

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,32 @@ import ToDoList from "./ToDoList";
 
 function App() {
 	let [count, setCount] = useState(0);
-	const clickButton= () =>{
+	const clickButton= () => {
 		setCount(++count);
 	}
+	let [money, setMoney] = useState([
+		{banknote: 'dollar', nominal: 100, number: ' a1234567890'},
+		{banknote: 'dollar', nominal: 50, number: ' z1234567890'},
+		{banknote: 'ruble', nominal: 100, number: ' w1234567890'},
+		{banknote: 'dollar', nominal: 100, number: ' e1234567890'},
+		{banknote: 'dollar', nominal: 50, number: ' c1234567890'},
+		{banknote: 'ruble', nominal: 100, number: ' r1234567890'},
+		{banknote: 'dollar', nominal: 50, number: ' x1234567890'},
+		{banknote: 'ruble', nominal: 50, number: ' v1234567890'}
+	]);
+	let filteredMoney = money;
+
+	let clickButtonFunction =(keyValue:string)=>{
+		console.log(keyValue)
+		if(keyValue === 'Ruble'){
+			filteredMoney = money.filter(el=>el.banknote === 'ruble')
+		}
+		if(keyValue === 'Dollar'){
+			filteredMoney = money.filter(el=>el.banknote === 'dollar')
+		}
+		setMoney(filteredMoney)
+	}
+
 	return (
 		<div className="App">
 			<Greeting/>
@@ -23,6 +46,23 @@ function App() {
 			<div>
 				<p>You clicked {count} times </p>
 				<button onClick={clickButton}>Click me</button>
+			</div>
+			<ul>
+				{filteredMoney.map((objFromMoneyArr, index)=>{
+					return (
+						<li key={index}>
+							<span>{objFromMoneyArr.banknote}</span>
+							<span>{objFromMoneyArr.nominal}</span>
+							<span>{objFromMoneyArr.number}</span>
+						</li>
+
+					)
+				})}
+			</ul>
+			<div>
+				<button onClick={()=>{clickButtonFunction("Ruble")}}>Ruble</button>
+				<button onClick={()=>{clickButtonFunction("Dollar")}}>Dollar</button>
+				<button onClick={()=>{clickButtonFunction("All")}}>All</button>
 			</div>
 
 		</div>
