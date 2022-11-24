8b2d69030624d76843c8ecea0b43fd5bf9e7104c
IJTINK7 <ijtink7@mail.ru>
2022-11-24 20:27:39 +0300

    Revert "Formatting the code"
    This reverts commit c837e9cb54861b94c288b0d414d673bb90cd1f75.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import './App.css';
 import Greeting from "./Greeting";
 import Rating from "./Rating";
 import ToDoList from "./ToDoList";
-import {TestFilterComponent} from "./TestFilterComponent";
 
 type FilterType = 'All' | 'Dollar' | 'Ruble';
-
 function App() {
 	let [count, setCount] = useState(0);
-	const clickButton = () => {
+	const clickButton= () => {
 		setCount(++count);
 	}
 	let money = [
@@ -22,16 +20,16 @@ function App() {
 		{banknote: 'dollar', nominal: 50, number: ' x1234567890'},
 		{banknote: 'ruble', nominal: 50, number: ' v1234567890'}
 	];
-	let [buttonValue, setButtonValue] = useState<FilterType>("All");
+	let[buttonValue, setButtonValue] = useState<FilterType>("All");
 	let filteredMoney = money;
-	if (buttonValue === 'Ruble') {
-		filteredMoney = money.filter(el => el.banknote === 'ruble')
+	if (buttonValue === 'Ruble'){
+		filteredMoney = money.filter(el=>el.banknote === 'ruble')
 	}
-	if (buttonValue === 'Dollar') {
-		filteredMoney = money.filter(el => el.banknote === 'dollar')
+	if (buttonValue === 'Dollar'){
+		filteredMoney = money.filter(el=>el.banknote === 'dollar')
 	}
 
-	let clickButtonFunction = (keyValue: FilterType) => {
+	let clickButtonFunction =(keyValue:FilterType)=>{
 		setButtonValue(keyValue);
 	}
 
@@ -51,7 +49,7 @@ function App() {
 				<button onClick={clickButton}>Click me</button>
 			</div>
 			<ul>
-				{filteredMoney.map((objFromMoneyArr, index) => {
+				{filteredMoney.map((objFromMoneyArr, index)=>{
 					return (
 						<li key={index}>
 							<span>{objFromMoneyArr.banknote}</span>
@@ -63,14 +61,13 @@ function App() {
 				})}
 			</ul>
 			<div>
-				<button onClick={() => {clickButtonFunction("Ruble")}}>Ruble</button>
-				<button onClick={() => {clickButtonFunction("Dollar")}}>Dollar</button>
-				<button onClick={() => {clickButtonFunction("All")}}>All</button>
+				<button onClick={()=>{clickButtonFunction("Ruble")}}>Ruble</button>
+				<button onClick={()=>{clickButtonFunction("Dollar")}}>Dollar</button>
+				<button onClick={()=>{clickButtonFunction("All")}}>All</button>
 			</div>
-			<TestFilterComponent/>
 
 		</div>
 	)
 }
-
+//Changing the path of repository again
 export default App;
