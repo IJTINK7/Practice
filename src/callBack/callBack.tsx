9ce0e6fd20eda4702adfb3402bd63e61f98ad858
IJTINK7 <ijtink7@mail.ru>
2022-12-08 23:32:33 +0300

    Connect callbacks to buttons

--- src/callBack/callBack.tsx
+++ src/callBack/callBack.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 export const User = ()=>{
+	const deleteUser=()=>{
+		alert("User should be deleted")
+	}
+	const saveUser=()=>{
+		alert("User should be saved")
+	}
 	return(
+
 		<div>
-			<button>Delete</button>
-			<button>Save</button>
+			<button onClick={deleteUser}>Delete</button>
+			<button onClick={saveUser}>Save</button>
 		</div>
 	);
 }
