559059763df9b5c4cc7bd2c09e95286221d04ae5
IJTINK7 <ijtink7@mail.ru>
2022-12-08 23:47:52 +0300

    Adding callback for textarea tag with onChange event

--- src/callBack/callBack.tsx
+++ src/callBack/callBack.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 export const User = ()=>{
 	const deleteUser=()=>{
-		alert("User should be deleted")
+		alert("User has been deleted")
 	}
 	const saveUser=()=>{
-		alert("User should be saved")
+		alert("User has been saved")
+	}
+	const nameChanged = () => {
+		console.log("Name has been changed")
 	}
 	return(
 
 		<div>
+			<textarea onChange={nameChanged}>Name</textarea>
 			<button onClick={deleteUser}>Delete</button>
 			<button onClick={saveUser}>Save</button>
 		</div>
