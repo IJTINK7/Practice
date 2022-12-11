aa24d9fb6c9ecd4376711018748c13b9a87dc43b
IJTINK7 <ijtink7@mail.ru>
2022-12-11 22:13:24 +0300

    Adding typification

--- src/07-destructuring-assignment/Destructuring.tsx
+++ src/07-destructuring-assignment/Destructuring.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import {ManType} from "./destructuring.test";
 type PropsType={
-
+	title: string;
+	man: ManType;
 }
 export const ManComponent = (props:PropsType)=>{
+
 	return(
-		<div>Hi</div>
+		<div>
+
+		</div>
 	);
 }
--- src/07-destructuring-assignment/destructuring.test.ts
+++ src/07-destructuring-assignment/destructuring.test.ts
@@ -1,9 +1,15 @@
 export {};
-test("testing", ()=>{
-	let man={
+export type ManType = {
+	name: string;
+	age: number;
+	lessons: Array<{ title: string }>
+}
+test("testing", () => {
+
+	let man = {
 		name: "Sasha",
 		age: 25,
-		lessons: [{title: "1"},{title: "2"}]
+		lessons: [{title: "1"}, {title: "2"}]
 	}
 	const age = man.age;
 	console.log(age)
