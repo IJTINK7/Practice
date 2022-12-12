36a81833c9c8aeec4a2f7b9ca8951d07941f5313
IJTINK7 <ijtink7@mail.ru>
2022-12-12 16:48:16 +0300

    Changing the style of container

--- src/OnOff/OnOff.tsx
+++ src/OnOff/OnOff.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import OnOffStyles from "./OnOff.module.css"
 type OnOffActiveType ={
 	isActive: boolean;
 }
 export const OnOff = (props: OnOffActiveType) =>{
-	const onOffStyle = {
+	const container ={
+		display:"flex"
+	}
+	const onStyle = {
 		width: "50px",
 		height: "50px",
 		border: "3px black solid",
 		marginRight: "10px",
+		marginBottom: "10px",
+		backgroundColor: "green",
+	};
+	const offStyle = {
+		width: "50px",
+		height: "50px",
+		border: "3px black solid",
+		marginRight: "10px",
+		marginBottom: "10px",
+		backgroundColor: "red",
 	};
 
 	const bulbStyle = {
@@ -16,11 +28,12 @@ export const OnOff = (props: OnOffActiveType) =>{
 		height: "50px",
 		borderRadius: "50%",
 		border: "3px black solid",
+		backgroundColor: "yellow",
 	};
 	return(
-		<div className ={OnOffStyles.container}>
-			<div style={onOffStyle}>On</div>
-			<div style={onOffStyle}>Off</div>
+		<div style={container}>
+			<div style={onStyle}>On</div>
+			<div style={offStyle}>Off</div>
 			<div style={bulbStyle}></div>
 		</div>
 	);
