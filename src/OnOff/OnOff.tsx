6ac4de22e1e0f8afbd4a7e7337b5b18cd3664908
IJTINK7 <ijtink7@mail.ru>
2022-12-12 16:36:41 +0300

    Changing the style of "On" and "Off" divs

--- src/OnOff/OnOff.tsx
+++ src/OnOff/OnOff.tsx
@@ -4,16 +4,23 @@ type OnOffActiveType ={
 	isActive: boolean;
 }
 export const OnOff = (props: OnOffActiveType) =>{
+	const onOffStyle = {
+		width: "50px",
+		height: "50px",
+		border: "3px black solid",
+		marginRight: "10px",
+	};
+
 	const bulbStyle = {
 		width: "50px",
 		height: "50px",
 		borderRadius: "50%",
 		border: "3px black solid",
-	}
+	};
 	return(
 		<div className ={OnOffStyles.container}>
-			<div className={OnOffStyles.On}>On</div>
-			<div className={OnOffStyles.Off}>Off</div>
+			<div style={onOffStyle}>On</div>
+			<div style={onOffStyle}>Off</div>
 			<div style={bulbStyle}></div>
 		</div>
 	);
