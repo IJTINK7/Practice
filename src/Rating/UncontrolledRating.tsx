776dbf1ef044bc4a436494ccc5ec07ef94c6b8b8
IJTINK7 <ijtink7@mail.ru>
2022-12-13 23:56:51 +0300

    Changing UI of buttons of Rating

--- src/Rating/UncontrolledRating.tsx
+++ src/Rating/UncontrolledRating.tsx
@@ -4,11 +4,18 @@ export const UncontrolledRating = () => {
 	const [value, setValue] = useState(0)
 	return (
 		<div>
-			<Star isActive={value > 0}/><button onClick={()=>{setValue(1)}}>1</button>
-			<Star isActive={value > 1}/><button onClick={()=>{setValue(2)}}>2</button>
-			<Star isActive={value > 2}/><button onClick={()=>{setValue(3)}}>3</button>
-			<Star isActive={value > 3}/><button onClick={()=>{setValue(4)}}>4</button>
-			<Star isActive={value > 4}/><button onClick={()=>{setValue(5)}}>5</button>
+			<Star isActive={value > 0}/>
+			<Star isActive={value > 1}/>
+			<Star isActive={value > 2}/>
+			<Star isActive={value > 3}/>
+			<Star isActive={value > 4}/>
+			<div>
+				<button onClick={()=>{setValue(1)}}>1</button>
+				<button onClick={()=>{setValue(2)}}>2</button>
+				<button onClick={()=>{setValue(3)}}>3</button>
+				<button onClick={()=>{setValue(4)}}>4</button>
+				<button onClick={()=>{setValue(5)}}>5</button>
+			</div>
 		</div>
 	);
 }
