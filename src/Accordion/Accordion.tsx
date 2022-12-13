0e524dfaab8b18971870d39bf743b9d043dcd781
IJTINK7 <ijtink7@mail.ru>
2022-12-13 23:33:57 +0300

    Changing the condition of rendering of Accordion

--- src/Accordion/Accordion.tsx
+++ src/Accordion/Accordion.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import {AccordionTitle} from "./AccordionTitle";
 import {AccordionBody} from "./AccordionBody";
-type AccordionType={
+
+type AccordionType = {
 	title: string;
 	collapsed: boolean
 }
-export const Accordion = (props:AccordionType) =>{
-	return(
-		<div>
-			<AccordionTitle title={props.title}/>
-			<AccordionBody collapsed={props.collapsed}/>
-		</div>
-	);
+export const Accordion = (props: AccordionType) => {
+	if (props.collapsed) {
+		return (
+			<div>
+				<AccordionTitle title={props.title}/>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<AccordionTitle title={props.title}/>
+				<AccordionBody collapsed={props.collapsed}/>
+			</div>
+		);
+	}
 }
