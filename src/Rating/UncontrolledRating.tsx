46e8cc3a96c76e977b8323c8dccab7ac1761e3b7
IJTINK7 <ijtink7@mail.ru>
2022-12-26 18:18:36 +0300

    Adding values for Star component

--- src/Rating/UncontrolledRating.tsx
+++ src/Rating/UncontrolledRating.tsx
@@ -4,18 +4,19 @@ export const UncontrolledRating = () => {
 	const [value, setValue] = useState(0)
 	return (
 		<div>
-			<Star isActive={value > 0} setValue={setValue}/>
-			<Star isActive={value > 1} setValue={setValue}/>
-			<Star isActive={value > 2} setValue={setValue}/>
-			<Star isActive={value > 3} setValue={setValue}/>
-			<Star isActive={value > 4} setValue={setValue}/>
+			<Star isActive={value > 0} setValue={setValue} value={1}/>
+			<Star isActive={value > 1} setValue={setValue} value={2}/>
+			<Star isActive={value > 2} setValue={setValue} value={3}/>
+			<Star isActive={value > 3} setValue={setValue} value={4}/>
+			<Star isActive={value > 4} setValue={setValue} value={5}/>
 		</div>
 	);
 }
 type StarPropsType = {
 	isActive: boolean;
+	value: 1 | 2 | 3 | 4 | 5
 	setValue: (value: 1 | 2 | 3 | 4 | 5)=>void;
 }
 const Star = (props: StarPropsType) => {
-	return <span onClick={()=>{props.setValue(5)}}>{props.isActive ? <b>Star </b> : "Star" }</span>
+	return <span onClick={()=>{props.setValue(props.value)}}>{props.isActive ? <b>Star </b> : "Star" }</span>
 }
