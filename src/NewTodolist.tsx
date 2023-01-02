442a9841669da84a830466b4a41229e1c19fd349
IJTINK7 <ijtink7@mail.ru>
2023-01-02 22:24:49 +0300

    Adding "All", "Active", "Completed" buttons of Todolist

--- src/NewTodolist.tsx
+++ src/NewTodolist.tsx
@@ -14,6 +14,11 @@ export const NewTodolist = () => {
 				<li><input type="checkbox" checked={true}/><span>CSS</span></li>
 				<li><input type="checkbox" checked={false}/><span>JS</span></li>
 			</div>
+			<div>
+				<button>All</button>
+				<button>Active</button>
+				<button>Completed</button>
+			</div>
 		</div>
 	);
 }
