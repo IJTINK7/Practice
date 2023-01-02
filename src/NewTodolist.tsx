9ee8f7796ad0389b57b3112c932ac6f30f90b66d
IJTINK7 <ijtink7@mail.ru>
2023-01-02 22:23:34 +0300

    Adding structure of Todolist

--- src/NewTodolist.tsx
+++ src/NewTodolist.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-export const NewTodolist =()=>{
-	return(
-		<div>Hi</div>
+export const NewTodolist = () => {
+	return (
+		<div>
+			<h3>What to learn</h3>
+			<div>
+				<input type="text"/>
+				<button>+</button>
+			</div>
+
+			<div>
+				<li><input type="checkbox" checked={true}/><span>HTML</span></li>
+				<li><input type="checkbox" checked={true}/><span>CSS</span></li>
+				<li><input type="checkbox" checked={false}/><span>JS</span></li>
+			</div>
+		</div>
 	);
 }
