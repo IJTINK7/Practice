062427fdba14bca488ac36d8b97ba8772c6f1fd6
IJTINK7 <ijtink7@mail.ru>
2023-01-04 08:33:02 +0300

    Refactoring the code

--- src/NewTodolist.tsx
+++ src/NewTodolist.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {FilterValuesType} from "./App";
 
- type TodolistType = {
+type TodolistType = {
 	title: string;
 	tasks: Array<TasksType>;
 	removeTask: (taskID: number) => void;
-	 changeFilter: (value: FilterValuesType) => void;
+	changeFilter: (value: FilterValuesType) => void;
 }
 export type TasksType = {
 	id: number;
@@ -35,9 +35,18 @@ export const NewTodolist = (props: TodolistType) => {
 				</ul>
 			</div>
 			<div>
-				<button onClick={()=>{props.changeFilter('all')}}>All</button>
-				<button onClick={()=>{props.changeFilter('active')}}>Active</button>
-				<button onClick={()=>{props.changeFilter('completed')}}>Completed</button>
+				<button onClick={() => {
+					props.changeFilter('all')
+				}}>All
+				</button>
+				<button onClick={() => {
+					props.changeFilter('active')
+				}}>Active
+				</button>
+				<button onClick={() => {
+					props.changeFilter('completed')
+				}}>Completed
+				</button>
 			</div>
 		</div>
 	);
