99a3a9c5c49f2c7be4e25cfb28ce524db6688fe8
IJTINK7 <ijtink7@mail.ru>
2023-01-06 06:26:19 +0300

    Commenting unnecessary code. Changing the structure of "NewTodolist" showing via map method for rendering 2 similar components, witch possible to change without binding to each other

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Greeting} from "./Greeting";
-import {Rating, RatingValueType} from "./Rating/Rating";
-import {ToDoList} from "./ToDoList";
-import {TestFilterComponent} from "./TestFilterComponent";
-import {Counter} from "./Counter";
-import {Currency} from "./Currency";
-import {NewComponent} from "./NewComponent";
-import {Input} from "./Input";
-import {OnOff} from "./OnOff/OnOff";
-import {User} from "./callBack/callBack";
-import {Accordion} from "./Accordion/Accordion";
-import {UncontrolledRating} from "./Rating/UncontrolledRating";
-import {UncontrolledAccordion} from "./Accordion/UncontrolledAccordion";
-import {UncontrolledOnOff} from "./OnOff/UncontrolledOnOff";
+// import {Greeting} from "./Greeting";
+// import {Rating, RatingValueType} from "./Rating/Rating";
+// import {ToDoList} from "./ToDoList";
+// import {TestFilterComponent} from "./TestFilterComponent";
+// import {Counter} from "./Counter";
+// import {Currency} from "./Currency";
+// import {NewComponent} from "./NewComponent";
+// import {Input} from "./Input";
+// import {OnOff} from "./OnOff/OnOff";
+// import {User} from "./callBack/callBack";
+// import {Accordion} from "./Accordion/Accordion";
+// import {UncontrolledRating} from "./Rating/UncontrolledRating";
+// import {UncontrolledAccordion} from "./Accordion/UncontrolledAccordion";
+// import {UncontrolledOnOff} from "./OnOff/UncontrolledOnOff";
 import {NewTodolist, TasksType} from "./NewTodolist";
 import {v1} from "uuid";
 
 export type FilterValuesType = "all" | "completed" | "active";
+type TodolistType={
+	id: string,
+	title: string,
+	filter: FilterValuesType
+}
 
 function App() {
-	const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
-	const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
-	const [switchOn, setSwitchOOn] = useState(false);
+	// const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+	// const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
+	// const [switchOn, setSwitchOOn] = useState(false);
 	const [tasks, setTasks] = useState<Array<TasksType>>([
 		{id: v1(), title: "HTML", isDone: true},
 		{id: v1(), title: "CSS", isDone: true},
@@ -57,34 +62,48 @@ function App() {
 	const changeFilter = (value: FilterValuesType) => {
 		setFilter(value);
 	}
-
+	let todoLists: Array<TodolistType>=[
+		{id:v1(), title: "What to learn", filter: "active"},
+		{id:v1(), title: "What to buy", filter: "completed"},
+	]
 	return (
 		<div className="App">
-			<Greeting/>
-			<ToDoList title={"What to learn?"}/>
-			<hr/>
-			<Rating value={ratingValue} onClick={setRatingValue}/>
-			<hr/>
-			<Currency/>
-			<hr/>
-			<TestFilterComponent/>
-			<hr/>
-			<Counter/>
-			<NewComponent/>
-			<Input/>
-			<OnOff on={switchOn} onChange={(on) => {
-				setSwitchOOn(on)
-			}}/>
-			<User/>
-			<Accordion title={"Menu"} collapsed={accordionCollapsed} onChange={() => {
-				setAccordionCollapsed(!accordionCollapsed)
-			}}/>
-			<UncontrolledRating/>
-			<UncontrolledAccordion title={"Burger"} collapsed={accordionCollapsed} onChange={() => {
-				setAccordionCollapsed(!accordionCollapsed)
-			}}/>
-			<UncontrolledOnOff/>
-			<NewTodolist title={"What to learn"} tasks={checkedTasks} removeTask={removeTask} addTask={addTask} changeFilter={changeFilter} changeStatus={changeStatus} filter={filter}/>
+			{/*<Greeting/>*/}
+			{/*<ToDoList title={"What to learn?"}/>*/}
+			{/*<hr/>*/}
+			{/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+			{/*<hr/>*/}
+			{/*<Currency/>*/}
+			{/*<hr/>*/}
+			{/*<TestFilterComponent/>*/}
+			{/*<hr/>*/}
+			{/*<Counter/>*/}
+			{/*<NewComponent/>*/}
+			{/*<Input/>*/}
+			{/*<OnOff on={switchOn} onChange={(on) => {*/}
+			{/*	setSwitchOOn(on)*/}
+			{/*}}/>*/}
+			{/*<User/>*/}
+			{/*<Accordion title={"Menu"} collapsed={accordionCollapsed} onChange={() => {*/}
+			{/*	setAccordionCollapsed(!accordionCollapsed)*/}
+			{/*}}/>*/}
+			{/*<UncontrolledRating/>*/}
+			{/*<UncontrolledAccordion title={"Burger"} collapsed={accordionCollapsed} onChange={() => {*/}
+			{/*	setAccordionCollapsed(!accordionCollapsed)*/}
+			{/*}}/>*/}
+			{/*<UncontrolledOnOff/>*/}
+			{
+				todoLists.map((el)=>{
+					return <NewTodolist title={el.title}
+										tasks={checkedTasks}
+										removeTask={removeTask}
+										addTask={addTask}
+										changeFilter={changeFilter}
+										changeStatus={changeStatus}
+										filter={el.filter}/>
+				})
+			}
+
 		</div>
 	)
 }
