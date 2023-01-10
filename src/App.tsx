1bf797f59baad1a8bfd628ed38b9d1782cadf50e
IJTINK7 <ijtink7@mail.ru>
2023-01-10 05:59:59 +0300

    Rewriting "changeStatus" function

--- src/App.tsx
+++ src/App.tsx
@@ -61,12 +61,8 @@ function App() {
 		setTasks({...tasks, [todolistID]: [task, ...tasks[todolistID]]})
 	}
 
-	function changeStatus(taskId: string, isDone: boolean) {
-		/*let task = tasks.find(t => t.id === taskId);
-		if (task) {
-			task.isDone = isDone;
-		}
-		setTasks([...tasks]);*/
+	function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
+		setTasks({...tasks, [todolistID]: tasks[todolistID].map(el=>el.id === taskId ? {...el, isDone} : el)})
 	}
 
 
