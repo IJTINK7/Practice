d403fb19c0995ac954e256c3c6b9f94bbbc8fd1d
IJTINK7 <ijtink7@mail.ru>
2023-01-16 18:20:24 +0300

    Starting of training of associative array

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ import './App.css';
 // import {UncontrolledRating} from "./Rating/UncontrolledRating";
 // import {UncontrolledAccordion} from "./Accordion/UncontrolledAccordion";
 // import {UncontrolledOnOff} from "./OnOff/UncontrolledOnOff";
-import {NewTodolist} from "./NewTodolist";
 import {v1} from "uuid";
+import {TodolistAssociativeArray} from "./associativeArray/TodolistAssociativeArray";
 
 export type FilterValuesType = "all" | "completed" | "active";
 type TodolistsType ={
@@ -27,13 +27,22 @@ function App() {
 	// const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
 	// const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
 	// const [switchOn, setSwitchOOn] = useState(false);
-	let todolistID1 = v1();
-	let todolistID2 = v1();
+	// let [tasks, setTasks] = useState([
+	//     {id: v1(), title: "HTML&CSS", isDone: true},
+	//     {id: v1(), title: "JS", isDone: true},
+	//     {id: v1(), title: "ReactJS", isDone: false},
+	//     {id: v1(), title: "Rest API", isDone: false},
+	//     {id: v1(), title: "GraphQL", isDone: false},
+	// ]);
+	// let [filter, setFilter] = useState<FilterValuesType>("all");
 
-	let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-		{id: todolistID1, title: "What to learn", filter: "all"},
-		{id: todolistID2, title: "What to buy", filter: "completed"},
-	]);
+	let todolistID1=v1();
+	let todolistID2=v1();
+
+	let [todolists, setTodolists] = useState<Array<todolistsType>>([
+		{id: todolistID1, title: 'What to learn', filter: 'all'},
+		{id: todolistID2, title: 'What to buy', filter: 'all'},
+	])
 
 	let [tasks, setTasks] = useState({
 		[todolistID1]:[
@@ -49,29 +58,40 @@ function App() {
 			{id: v1(), title: "ReactJS2", isDone: false},
 			{id: v1(), title: "Rest API2", isDone: false},
 			{id: v1(), title: "GraphQL2", isDone: false},
-		],
+		]
 	});
 
-	function removeTask(todolistID: string, taskID: string) {
-		setTasks({...tasks, [todolistID]: tasks[todolistID].filter(el=> el.id !== taskID)})
+	function removeTask(id: string) {
+		let filteredTasks = tasks.filter(t => t.id != id);
+		setTasks(filteredTasks);
 	}
 
-	function addTask(todolistID: string, title: string) {
+	function addTask(title: string) {
 		let task = {id: v1(), title: title, isDone: false};
-		setTasks({...tasks, [todolistID]: [task, ...tasks[todolistID]]})
+		let newTasks = [task, ...tasks];
+		setTasks(newTasks);
 	}
 
-	function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
-		setTasks({...tasks, [todolistID]: tasks[todolistID].map(el=>el.id === taskId ? {...el, isDone} : el)})
+	function changeStatus(taskId: string, isDone: boolean) {
+		let task = tasks.find(t => t.id === taskId);
+		if (task) {
+			task.isDone = isDone;
+		}
+
+		setTasks([...tasks]);
 	}
 
+	let tasksForTodolist = tasks;
 
-	function changeFilter(todolistID: string, value: FilterValuesType) {
-		setTodolists(todolists.map(el=>el.id === todolistID ? {...el, filter: value}: el))
+	if (filter === "active") {
+		tasksForTodolist = tasks.filter(t => t.isDone === false);
 	}
-	const removeTodolist =(todolistID: string)=>{
-		setTodolists(todolists.filter(el=>el.id !== todolistID))
-		delete tasks[todolistID];
+	if (filter === "completed") {
+		tasksForTodolist = tasks.filter(t => t.isDone === true);
+	}
+
+	function changeFilter(value: FilterValuesType) {
+		setFilter(value);
 	}
 	return (
 		<div className="App">
@@ -99,29 +119,14 @@ function App() {
 			{/*	setAccordionCollapsed(!accordionCollapsed)*/}
 			{/*}}/>*/}
 			{/*<UncontrolledOnOff/>*/}
-			{todolists.map(el=>{
-				let tasksForTodolist = tasks[el.id];
-
-				if (el.filter === "active") {
-					tasksForTodolist = tasks[el.id].filter(t => !t.isDone);
-				}
-				if (el.filter === "completed") {
-					tasksForTodolist = tasks[el.id].filter(t => t.isDone);
-				}
-
-				return (
-				<NewTodolist key={el.id}
-							 todolistID={el.id}
-							 title={el.title}
-							 tasks={tasksForTodolist}
-							 removeTask={removeTask}
-							 changeFilter={changeFilter}
-							 addTask={addTask}
-							 changeTaskStatus={changeStatus}
-							 filter={el.filter}
-							 removeTodolist={removeTodolist}
-				/>
-			)})}
+			<TodolistAssociativeArray title="What to learn"
+							  tasks={tasksForTodolist}
+							  removeTask={removeTask}
+							  changeFilter={changeFilter}
+							  addTask={addTask}
+							  changeTaskStatus={changeStatus}
+							  filter={filter}
+					/>
 		</div>
 	)
 }
