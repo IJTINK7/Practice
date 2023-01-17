0bacae522b8151cb02642667cc415c92b6965c8f
IJTINK7 <ijtink7@mail.ru>
2023-01-17 19:35:53 +0300

    Fixing "changeStatus" function

--- src/App.tsx
+++ src/App.tsx
@@ -73,13 +73,8 @@ function App() {
 		setTasks({...tasks, [todolistID]: [newTask, ...tasks[todolistID] ]});
 	}
 
-	function changeStatus(taskId: string, isDone: boolean) {
-		// let task = tasks.find(t => t.id === taskId);
-		// if (task) {
-		// 	task.isDone = isDone;
-		// }
-		//
-		// setTasks([...tasks]);
+	function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
+		setTasks({...tasks, [todolistID]: tasks[todolistID].map(el=> el.id === taskId ? {...el, isDone: isDone} : el)})
 	}
 
 	function changeFilter(todolistID: string, value: FilterValuesType) {
