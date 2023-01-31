27d1b0bf940845e1d18aae5a7531fdbc318627c0
IJTINK7 <ijtink7@mail.ru>
2023-01-31 19:32:55 +0300

    Changing structure of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,30 @@ type TodolistsType = {
 }
 
 function App() {
-	let todolistID1 = v1();
-	let todolistID2 = v1();
+	let todolistID1=v1();
+	let todolistID2=v1();
 
 	let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-		{id: todolistID1, title: 'What to learn', filter: 'active'},
-		{id: todolistID2, title: 'What to buy', filter: 'completed'},
+		{id: todolistID1, title: 'What to learn', filter: 'all'},
+		{id: todolistID2, title: 'What to buy', filter: 'all'},
 	])
 
-	let [tasks, setTasks] = useState([
-		{id: v1(), title: "HTML&CSS", isDone: true},
-		{id: v1(), title: "JS", isDone: true},
-		{id: v1(), title: "ReactJS", isDone: false},
-		{id: v1(), title: "Rest API", isDone: false},
-		{id: v1(), title: "GraphQL", isDone: false},
-	]);
+	let [tasks, setTasks] = useState({
+		[todolistID1]:[
+			{id: v1(), title: "HTML&CSS", isDone: true},
+			{id: v1(), title: "JS", isDone: true},
+			{id: v1(), title: "ReactJS", isDone: false},
+			{id: v1(), title: "Rest API", isDone: false},
+			{id: v1(), title: "GraphQL", isDone: false},
+		],
+		[todolistID2]:[
+			{id: v1(), title: "HTML&CSS2", isDone: true},
+			{id: v1(), title: "JS2", isDone: true},
+			{id: v1(), title: "ReactJS2", isDone: false},
+			{id: v1(), title: "Rest API2", isDone: false},
+			{id: v1(), title: "GraphQL2", isDone: false},
+		]
+	})
 
 	function removeTask(id: string) {
 		// let filteredTasks = tasks.filter(t => t.id !== id);
