77c2e9e3bc62fcea26fae79c04b7c3e04b99d1cd
IJTINK7 <ijtink7@mail.ru>
2023-01-31 18:34:28 +0300

    Adding Accordion.tsx file

---
+++ src/Accordion/Accordion.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {AccordionTitle} from "./AccordionTitle";
+import {AccordionBody} from "./AccordionBody";
+
+type AccordionType = {
+	title: string;
+	collapsed: boolean;
+	onChange:()=>void;
+}
+export const Accordion = (props: AccordionType) => {
+	return (
+		<div>
+			<AccordionTitle title={props.title} onChange={props.onChange}/>
+			{!props.collapsed && <AccordionBody/>}
+		</div>
+	);
+}
