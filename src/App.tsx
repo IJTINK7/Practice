ae45306a122d097fe6e7d05ec38fa70af71f44e3
IJTINK7 <ijtink7@mail.ru>
2023-02-02 16:25:22 +0300

    Fixing TodolistsType

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,16 @@ import {v1} from 'uuid';
 import {TaskType,Todolist} from "./Todolist";
 
 export type FilterValuesType = "all" | "active" | "completed";
-type TodolistsType = { id: string, title: string, filter: FilterValuesType }
+type TodolistsType = { id: string, title: string}
 type TasksType = {
 	[key: string]: TaskType[]
 }
 
 function App() {
-	// let todolistID1 = v1();
-	// let todolistID2 = v1();
-	//
-	// let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-	//     {id: todolistID1, title: 'What to learn', filter: 'all'}, //0
-	//      {id: todolistID2, title: 'What to buy', filter: 'all'},  //1
-	// ])
-	//
-	// let [tasks, setTasks] = useState({
-	//     [todolistID1]: [
-	//         {id: v1(), title: "HTML&CSS", isDone: true},
-	//         {id: v1(), title: "JS", isDone: true},
-	//         {id: v1(), title: "ReactJS", isDone: false},
-	//         {id: v1(), title: "Rest API", isDone: false},
-	//         {id: v1(), title: "GraphQL", isDone: false},
-	//     ],
-	//     [todolistID2]: [
-	//         {id: v1(), title: "HTML&CSS2", isDone: true},
-	//         {id: v1(), title: "JS2", isDone: true},
-	//         {id: v1(), title: "ReactJS2", isDone: false},
-	//         {id: v1(), title: "Rest API2", isDone: false},
-	//         {id: v1(), title: "GraphQL2", isDone: false},
-	//     ]
-	// });
-
 	let todolistId1 = v1();
 	let todolistId2 = v1();
 
-	let [todolists, setTodolists] = useState<Array<TodolistType>>([
+	let [todolists, setTodolists] = useState<Array<TodolistsType>>([
 		{id: todolistId1, title: "What to learn"},
 		{id: todolistId2, title: "What to buy"}
 	])
@@ -60,13 +35,11 @@ function App() {
 		}
 	});
 
-
 	const removeTodolist = (todolistId: string) => {
 		setTodolists(todolists.filter(el => el.id !== todolistId))
 		delete tasks[todolistId]
 		console.log(tasks)
 	}
-
 
 	function removeTask(todolistId: string, taskId: string) {
 		setTasks({...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== taskId)})
