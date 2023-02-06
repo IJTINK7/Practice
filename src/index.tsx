9f98d3532a06ae8e869e6f9b6524eb2798b841c1
IJTINK7 <ijtink7@mail.ru>
2023-02-06 23:26:45 +0300

    Changing index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // @ts-ignore
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
