b56d0a8a8d3e2fbe1b56dd68dd01a05249d7f042
IJTINK7 <ijtink7@mail.ru>
2023-02-07 23:44:47 +0300

    Deleting some Routes

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -19,8 +19,7 @@ export const Site = () => {
                 <div className={styles.content}>
                     <Routes>
                         <Route path={"/page1"} element={<PageOne/>}/>
-                        <Route path={"/page2"} element={<PageTwo/>}/>
-                        <Route path={"/page3"} element={<PageThree/>}/>
+
                         <Route path={"/*"} element={<Error404/>}/>
                     </Routes>
                 </div>
