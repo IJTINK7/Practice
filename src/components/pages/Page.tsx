acdcb246eadbd9c753d56dba64fea6810b580819
IJTINK7 <ijtink7@mail.ru>
2023-02-08 23:05:54 +0300

    Adding typification for Routes

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-export const Page = () => {
+import {PagesType} from "../../data/dataState";
+export type PropsType={
+	pages: Array<PagesType>
+}
+export const Page = (props:PropsType) => {
 	return(
 		<div>
 
