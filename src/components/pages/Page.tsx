a25be07dff6d4259c9094cc8ec045070970b3446
IJTINK7 <ijtink7@mail.ru>
2023-02-08 23:09:01 +0300

    Adding useParams hook

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {PagesType} from "../../data/dataState";
+import {useParams} from "react-router-dom";
 export type PropsType={
 	pages: Array<PagesType>
 }
 export const Page = (props:PropsType) => {
+	const params = useParams()
 	return(
 		<div>
 
