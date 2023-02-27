daead90243245cb02dda5d1057ff6043e9f56698
IJTINK7 <ijtink7@mail.ru>
2023-02-27 10:55:14 +0300

    Using Page.tsx component as only component for showing content

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import styles from './Site.module.css'
-import {PageOne} from "./pages/PageOne";
-import {PageTwo} from "./pages/PageTwo";
-import {PageThree} from "./pages/PageThree";
+// import {PageOne} from "./pages/PageOne";
+// import {PageTwo} from "./pages/PageTwo";
+// import {PageThree} from "./pages/PageThree";
 import {Error404} from "./pages/Error404";
+import {dataState} from "../data/dataState";
+import {Page} from "./pages/Page";
 
 
 export const Site = () => {
@@ -13,16 +15,19 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page1'}>Page 1</NavLink></div>
-                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page2'}>Page 2</NavLink></div>
-                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page3'}>Page 3</NavLink></div>
+                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page0'}>Page 1</NavLink></div>
+                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page1'}>Page 2</NavLink></div>
+                    <div><NavLink className={ ({isActive}) => isActive ? styles.active : styles.navLink } to={'/page2'}>Page 3</NavLink></div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path={"/"} element={<Navigate to={"/page1"}/>}/>
-                        <Route path={"/page1"} element={<PageOne/>}/>
-                        <Route path={"/page2"} element={<PageTwo/>}/>
-                        <Route path={"/page3"} element={<PageThree/>}/>
+
+                        <Route path={"/page/:id"} element={<Page pages={dataState.pages}/>}/>
+
+                        {/*<Route path={"/page1"} element={<PageOne/>}/>*/}
+                        {/*<Route path={"/page2"} element={<PageTwo/>}/>*/}
+                        {/*<Route path={"/page3"} element={<PageThree/>}/>*/}
                         <Route path={"/*"} element={<Error404/>}/>
                     </Routes>
                 </div>
